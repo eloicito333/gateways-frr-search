87b1cef890835b3ce50de9ba29cfd246d847305f
eloicito333 <eloixxl2008@gmail.com>
2022-09-08 21:14:20 +0200

    express-sslify included

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const Corrosion = require('corrosion');
-const dotenv = require('dotenv')
+const http = require('http');
+const enforce = require('express-sslify');
+const dotenv = require('dotenv');
 
 dotenv.config()
 
@@ -9,6 +11,10 @@ const proxy = new Corrosion({
     codec: 'xor', // apply basic xor encryption to url parameters in an effort to evade filters. Optional.
     prefix: '/get/' // specify the endpoint (prefix). Optional.
 });
+
+const server = http.createServer(app)
+
+app.use(enforce.HTTPS());
 
 proxy.bundleScripts();
 
@@ -22,4 +28,4 @@ app.post(/\/get\/*/, (req, res) => {
 
 app.on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Server running at port: ${process.env.PORT}`))
+server.listen(process.env.PORT || 8080, () => console.log(`Server running at port: ${process.env.PORT}`))
